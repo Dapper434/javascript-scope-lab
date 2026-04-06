d7c65f6904998b92d510497e947fe33e23189c02
Pipsy <gpflaslik7112@gmail.com>
2026-04-06 21:56:45 +0300

    initial commit of js code

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+//  Global scope variables
+var burgers = ['Hamburger', 'Cheeseburger'];
+var featuredDrink = 'Strawberry Milkshake';
+
+// Function-scoped variable
+function addBurger() {
+  var newBurger = 'Flatburger';        // function-scoped (var)
+  burgers.push(newBurger);
+}
+
+//  Block-scoped variable
+if (true) {
+  let anotherNewBurger = 'Maple Bacon Burger';   // block-scoped (let)
+  burgers.push(anotherNewBurger);
+}
+
+//  Change featured drink
+function changeFeaturedDrink() {
+  featuredDrink = 'The JavaShake';
+}
